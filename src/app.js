e4f77e7f8946212a680c8eb9a4b30e602e1e3b0c
Yaremi Astorga <yare1894@gmail.com>
2025-01-20 21:52:36 -0700

    feat(app): add private route to get users list

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import passport from './config/passport.js';
 import errorHandler from './middleware/errorHandler.js';
 import authRoutes from './routes/authRoutes.js';
+import privateRoutes from './routes/privateRoutes.js';
 
 const app = express();
 app.use(cookieParser());
@@ -16,6 +17,7 @@ app.use(passport.initialize());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/private', privateRoutes);
 
 // Error handling
 app.use(errorHandler);
